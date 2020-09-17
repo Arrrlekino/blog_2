a9882eb95aabc553b3f2b9b049166e699c5c9db7
arrrlekino <zebrasteel@gmail.com>
2020-09-17 17:40:07 +0200

    add posts/edit and handler for

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,18 +2,34 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all()
   end
-  
+
   def show
     @post = Post.find(params[:id])
-    
+  end
+
+  def edit
+    @topics = Topic.all()
+    @post = Post.find(params[:id])
   end
 
   def new
     @topics = Topic.all()
   end
-   def create
-     @post = Post.new(params.require(:post).permit(:title, :body, :topic_id))
-     @post.save
-     redirect_to '/posts/' + @post.id.to_s
-   end
+
+  def create
+   # @post = Post.new(params.require(:post).permit(:title, :body, :topic_id))
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to '/posts/' + @post.id.to_s
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to '/posts/' + @post.id.to_s
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :topic_id)
+  end
 end
