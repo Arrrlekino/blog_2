c24f106a08a9fd042ffa0e8e0217e28467fd1fac
arrrlekino <zebrasteel@gmail.com>
2020-09-21 20:08:03 +0200

    add comments_controller +routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get 'posts/edit/:id', to: 'posts#edit'
   post 'posts/update/:id', to: 'posts#update'
 
+  post 'comments/create/:post_id', to: 'comments#create', constraints: { post_id: /\d+/ }
+  post 'comments/create/:post_id/replies/:parent_id', to: 'comments#create', constraints: { post_id: /\d+/, parent_id: /\d+/ }
+
   get 'posts', to: 'posts#index'
   root to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
